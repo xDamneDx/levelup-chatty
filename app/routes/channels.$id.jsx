9075fc9df37d1f8fd90d8e51f2cbfcfd7ae70cf8
xDamneDx <xdamnedx@gmail.com>
2023-08-22 16:03:05 +0200

    add supabase data mutation w/ remix action

--- app/routes/channels.$id.jsx
+++ app/routes/channels.$id.jsx
@@ -1,10 +1,10 @@
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import supabase from "~/utils/supabase";
 
 export const loader = async ({ params: { id } }) => {
   const { data: channel, error } = await supabase
     .from("channels")
-    .select("*, messages(id, content)")
+    .select("id, title, messages(id, content)")
     .match({ id })
     .single();
 
@@ -15,8 +15,31 @@ export const loader = async ({ params: { id } }) => {
   return { channel };
 };
 
+export const action = async ({ request, params: { id: channel_id } }) => {
+  const formData = await request.formData();
+  const content = formData.get("content");
+
+  const { error } = await supabase
+    .from("messages")
+    .insert({ content, channel_id });
+
+  if (error) {
+    console.error(error.message);
+  }
+
+  return null;
+};
+
 export default function ChannelRoute() {
   const { channel } = useLoaderData();
 
-  return <p>{channel.title}</p>;
+  return (
+    <div>
+      <pre>{JSON.stringify(channel, null, 2)}</pre>
+      <Form method="post">
+        <input type="text" name="content" />
+        <button>Send!</button>
+      </Form>
+    </div>
+  );
 }
